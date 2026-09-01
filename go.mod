34b24bd7eb0b355aa9da125fceefd889d3bb90a2
dinesheth <dinesheth41@gmail.com>
2026-09-01 23:05:34 +0530

    add dotenv

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/dinesheth/weather-agent
 go 1.26.7
 
 require github.com/gage-technologies/mistral-go v1.1.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	apikey := ""
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error load .env file")
+	}
+	apikey := os.Getenv("APIKEY")
 	model := "mistral-tiny"
 	client := New(apikey, model)
-	_, err := client.message("Hi, What is the date today!")
+	res, err := client.message("Hi, What is the date today!")
 	if err != nil {
 		log.Println("error:", err)
 	}
+
+	fmt.Println("res:", res)
+
 }
